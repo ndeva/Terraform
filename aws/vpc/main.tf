67ad6fc0ece3770c7c0ff2c6ce9f32795545f194
Brian Ndeva Jumba <ndevabn@gmail.com>
2023-03-30 18:47:13 +0000

    changed destination cidr block for routing

--- aws/vpc/main.tf
+++ aws/vpc/main.tf
@@ -72,6 +72,6 @@ resource "aws_eip" "nat" {
 # create a default route in the private route table to use the NAT gateway
 resource "aws_route" "private" {
   route_table_id = aws_route_table.private.id
-  cidr_block = "0.0.0.0/0"
+  destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.brivantech.id
 }
